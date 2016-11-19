6e91f2ea8f38bf02836336b5096c3caa9ad364c4
rene <rene@epispectrum.de>
2016-11-19 13:25:43 +0100

    Add ability to define a tree structure in a multimap.

---
+++ include/kompositum/builder.h
@@ -0,0 +1,31 @@
+// Copyright(c) 2016 René Hansen.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#pragma once
+
+#include <kompositum/visitor.h>
+#include <kompositum/component.h>
+#include <kompositum/composite.h>
+#include <kompositum/leaf.h>
+#include <map>
+
+namespace Kompositum {
+using ParentChildTreeDef = std::multimap<IDType, IDType>;
+
+ComponentPtr buildComposite(IDType id, const ParentChildTreeDef& treeMap) {
+    bool hasChildren = treeMap.find(id) != treeMap.end();
+    if (hasChildren) {
+        auto composite = std::make_unique<Composite>(id);
+
+        auto range = treeMap.equal_range(id);
+        for (auto child = range.first; child != range.second; ++child) {
+            composite->addChild(buildComposite(child->second, treeMap));
+        }
+
+        return std::move(composite);
+    }
+
+    return std::make_unique<Leaf>(id);
+}
+
+} // namespace Kompositum
